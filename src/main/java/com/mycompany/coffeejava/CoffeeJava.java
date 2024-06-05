e7e4fc4d3b5d0c13d293d67a72d7c449779f5db5
Elias <eliasmanica@poli.ufrj.br>
2024-06-04 21:34:11 -0300

    feat: create repository

---
+++ src/main/java/com/mycompany/coffeejava/CoffeeJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coffeejava;
+
+import telas.FramePrincipal;
+
+/**
+ *
+ * @author Elias
+ */
+public class CoffeeJava {
+
+    public static void main(String[] args) {
+        new FramePrincipal().setVisible(true);
+    }
+}
